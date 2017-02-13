dfc8382a1ad341557194a23461e6dddb283108cb
Aleksandar Damjanovic <codejanovic@gmail.com>
2017-02-13 22:32:54 +0100

    added project files and updated README.md

---
+++ src/main/java/io/github/codejanovic/java/lazy/ThrowingLazy.java
@@ -0,0 +1,37 @@
+package io.github.codejanovic.java.lazy;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public interface ThrowingLazy<T> {
+    T value() throws Exception;
+
+    final class Value<T> implements ThrowingLazy<T> {
+        private final ThrowingLazy<T> lazy;
+
+        public Value(final ThrowingLazy<T> supplier) {
+            this.lazy = supplier;
+        }
+
+        @Override
+        public T value() throws Exception {
+            return lazy.value();
+        }
+    }
+
+    final class Cached<T> implements ThrowingLazy<T> {
+        private final Queue<T> cache = new ArrayBlockingQueue<>(1);
+        private final ThrowingLazy<T> lazy;
+
+        public Cached(final ThrowingLazy<T> lazy) {
+            this.lazy = lazy;
+        }
+
+        @Override
+        public T value() throws Exception {
+            if (cache.isEmpty())
+                cache.add(lazy.value());
+            return cache.peek();
+        }
+    }
+}
---
+++ src/test/java/io/github/codejanovic/java/lazy/LazyTest.java
@@ -0,0 +1,34 @@
+package io.github.codejanovic.java.lazy;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LazyTest {
+    @Test
+    public void testSmartLazy() throws Exception {
+        final List<String> list = new ArrayList<>();
+        final Lazy<String> lazy = new Lazy.Value<>(() -> { list.add("1"); return "1"; });
+        assertThat(list).isEmpty();
+
+        assertThat(lazy.value()).isEqualTo("1");
+        assertThat(list).hasSize(1);
+        assertThat(lazy.value()).isEqualTo("1");
+        assertThat(list).hasSize(2);
+    }
+
+    @Test
+    public void testCachedLazy() throws Exception {
+        final List<String> list = new ArrayList<>();
+        final Lazy<String> lazy = new Lazy.Cached<>(new Lazy.Value<>(() -> { list.add("1"); return "1"; }));
+        assertThat(list).isEmpty();
+
+        assertThat(lazy.value()).isEqualTo("1");
+        assertThat(list).hasSize(1);
+        assertThat(lazy.value()).isEqualTo("1");
+        assertThat(list).hasSize(1);
+    }
+}
