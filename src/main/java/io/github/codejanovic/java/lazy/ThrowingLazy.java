7abe5e458696edf15a402873d37146792e70f6be
codejanovic <codejanovic@gmail.com>
2017-10-28 12:26:11 +0200

    added Supplier<T> compatibility and generified ThrowingLazy

--- src/main/java/io/github/codejanovic/java/lazy/ThrowingLazy.java
+++ src/main/java/io/github/codejanovic/java/lazy/ThrowingLazy.java
@@ -3,32 +3,32 @@ package io.github.codejanovic.java.lazy;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
-public interface ThrowingLazy<T> {
-    T value() throws Exception;
+public interface ThrowingLazy<T, E extends Exception> {
+    T value() throws E;
 
-    final class Value<T> implements ThrowingLazy<T> {
-        private final ThrowingLazy<T> lazy;
+    final class Value<T, E extends Exception> implements ThrowingLazy<T, E> {
+        private final ThrowingLazy<T, E> lazy;
 
-        public Value(final ThrowingLazy<T> supplier) {
+        public Value(final ThrowingLazy<T, E> supplier) {
             this.lazy = supplier;
         }
 
         @Override
-        public T value() throws Exception {
+        public T value() throws E {
             return lazy.value();
         }
     }
 
-    final class Cached<T> implements ThrowingLazy<T> {
+    final class Cached<T, E extends Exception> implements ThrowingLazy<T, E> {
         private final Queue<T> cache = new ArrayBlockingQueue<>(1);
-        private final ThrowingLazy<T> lazy;
+        private final ThrowingLazy<T, E> lazy;
 
-        public Cached(final ThrowingLazy<T> lazy) {
+        public Cached(final ThrowingLazy<T, E> lazy) {
             this.lazy = lazy;
         }
 
         @Override
-        public T value() throws Exception {
+        public T value() throws E {
             if (cache.isEmpty())
                 cache.add(lazy.value());
             return cache.peek();
--- src/test/java/io/github/codejanovic/java/lazy/LazyTest.java
+++ src/test/java/io/github/codejanovic/java/lazy/LazyTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LazyTest {
     @Test
@@ -23,9 +24,58 @@ public class LazyTest {
     @Test
     public void testCachedLazy() throws Exception {
         final List<String> list = new ArrayList<>();
-        final Lazy<String> lazy = new Lazy.Cached<>(new Lazy.Value<>(() -> { list.add("1"); return "1"; }));
+        final Lazy<String> lazy = new Lazy.Cached<>(
+                new Lazy.Value<>(() -> {
+                    list.add("1");
+                    return "1";
+                }));
         assertThat(list).isEmpty();
 
+        assertThat(lazy.value()).isEqualTo("1");
+        assertThat(list).hasSize(1);
+        assertThat(lazy.value()).isEqualTo("1");
+        assertThat(list).hasSize(1);
+    }
+
+    @Test
+    public void testThrowingLazy() throws Exception {
+        final List<String> list = new ArrayList<>();
+        final ThrowingLazy<String, ArrayIndexOutOfBoundsException> lazy =
+                new ThrowingLazy.Value<>(() -> {
+                    throw new ArrayIndexOutOfBoundsException("");
+                });
+
+        assertThat(list).isEmpty();
+        assertThatThrownBy(lazy::value).isInstanceOf(ArrayIndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void testThrowingCachedLazyWillThrow() throws Exception {
+        final List<String> list = new ArrayList<>();
+        final ThrowingLazy<String, ArrayIndexOutOfBoundsException> lazy =
+                new ThrowingLazy.Cached<>(
+                        new ThrowingLazy.Value<>(() -> {
+                            throw new ArrayIndexOutOfBoundsException("");
+                        })
+                );
+
+        assertThat(list).isEmpty();
+        assertThatThrownBy(lazy::value).isInstanceOf(ArrayIndexOutOfBoundsException.class);
+        assertThatThrownBy(lazy::value).isInstanceOf(ArrayIndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void testThrowingCachedLazy() throws Exception {
+        final List<String> list = new ArrayList<>();
+        final ThrowingLazy<String, ArrayIndexOutOfBoundsException> lazy =
+                new ThrowingLazy.Cached<>(
+                        new ThrowingLazy.Value<>(() -> {
+                            list.add("1");
+                            return "1";
+                        })
+                );
+
+        assertThat(list).isEmpty();
         assertThat(lazy.value()).isEqualTo("1");
         assertThat(list).hasSize(1);
         assertThat(lazy.value()).isEqualTo("1");
